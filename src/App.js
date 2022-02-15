4ccbcd2d6dd52237333dd1053ae51a838bab1bb0
Abhishek EH <collegewap@gmail.com>
2022-02-15 10:22:18 +0530

    displaying tasks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import {
   Button,
   Card,
-  Checkbox,
   ControlGroup,
   Elevation,
   InputGroup,
   Tag,
 } from "@blueprintjs/core";
 import { useState } from "react";
-import useLocalStorage from "./useLocalStorage";
 
 function App() {
   const [userInput, setUserInput] = useState("");
-  const [todoList, setTodoList] = useLocalStorage("todo-items", []);
+
+  const [todoList, setTodoList] = useState([]);
 
   const addItem = (e) => {
     e.preventDefault();
@@ -26,19 +25,6 @@ function App() {
     }
   };
 
-  const toggleTask = (index) => {
-    setTodoList((existingItems) =>
-      existingItems.map((item, i) =>
-        index === i ? { ...item, finished: !item.finished } : item
-      )
-    );
-  };
-
-  const deleteTask = (index) => {
-    setTodoList((existingItems) =>
-      existingItems.filter((item, i) => index !== i)
-    );
-  };
   return (
     <div className="App">
       <Card elevation={Elevation.TWO}>
@@ -50,26 +36,15 @@ function App() {
               value={userInput}
               onChange={(e) => setUserInput(e.target.value)}
             />
-            <Button intent="primary">Add</Button>
+            <Button type="submit" intent="primary">
+              Add
+            </Button>
           </ControlGroup>
         </form>
         <div className="items-list">
           {todoList.map((item, index) => (
-            <Tag
-              key={index + item.name}
-              large
-              minimal
-              multiline
-              onRemove={() => deleteTask(index)}
-            >
-              <Checkbox
-                checked={item.finished}
-                onChange={() => toggleTask(index)}
-              >
-                <span className={item.finished ? "finished" : ""}>
-                  {item.name}
-                </span>
-              </Checkbox>
+            <Tag key={index + item.name} large minimal multiline>
+              <span>{item.name}</span>
             </Tag>
           ))}
         </div>
